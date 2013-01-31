05cadb6359b250aa72d1317e9418e2c9b2bd1b57
Nick MBA <nick.tomlin@gmail.com>
2013-01-30 20:12:33 -0600

    BLJF

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/rand', function(req, response){
       // response.send( '<img src="http://bukk.it/'+ bukkits[randBukkit] + "\">");
      // response.send( '<img src="http://bukk.it/'+ bukkits[randBukkit] + "\">");
 
-     response.send('<img src="http://bukk.it/clint.jpg' + bukkits[1]  + '"> );
+     response.send('<img src="http://bukk.it/clint.jpg' + bukkits[1]  + '\">' );
   });
 
 });
