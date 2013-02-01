e3226b767b839b453c73c2cd3f9bf76204215a37
Nick Tomlin <nick.tomlin@gmail.com>
2013-02-01 16:31:05 -0600

    added no cache headers, to no avail.

--- app.js
+++ app.js
@@ -16,8 +16,14 @@ function rand(min,max){
 
 var app = express();
 
-
 app.get('/', function(req, response){
+
+  response.send("Index. Dawg.")
+
+});
+
+
+app.get('/rand', function(req, response){
 
   var bukkits = []; // hold our bukkit objects
   var url = 'http://bukk.it/'; // bukk.it
@@ -34,16 +40,16 @@ app.get('/', function(req, response){
       return
     }
     //  use our randNum function
-      var randomBukkit = 'http://bukk.it/' + bukkits[rand(1,bukkits.length)] ;
-      response.redirect(303,randomBukkit);
+      var randomBukkit = bukkits[rand(1,bukkits.length)];
+      var returnBukkit = 'http://bukk.it/' +randomBukkit;
+      response.header("Cache-Control", "no-cache, no-store, must-revalidate");
+      response.header("Pragma", "no-cache");
+      response.header("Expires", 0);
+      // perhaps you can set the mime type here by response.type('image/' + randomBukket.slice([randombukkit.length -4]) )
+      response.redirect(307,returnBukkit);
   });
 });
 
-app.get('/hello', function(req, response){
-
-  response.send("Index. Dawg.")
-
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
