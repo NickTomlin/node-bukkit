0eae91a34821c6f35fc00f6028b2cb5f354876ef
Nick Tomlin <nick.tomlin@gmail.com>
2013-04-12 15:31:06 -0500

    reworked bukkits slightly; began introducing helper functions

--- bukkits.js
+++ bukkits.js
@@ -1,6 +1,12 @@
-var request = require('request');
-var cheerio = require('cheerio');
+var request = require('request')
+, cheerio = require('cheerio')
+;
 
+/**
+ * Scrapes bukk.it for image urls, stores them in an array and passes that to a callback.
+ * @param  {Function} callback a callback function that accepts an array of bukkits
+ *
+ */
 exports.get = function get(callback) {
 
   var bukkitData = []; // hold our bukkit objects
@@ -8,17 +14,29 @@ exports.get = function get(callback) {
   var targets = 'td a';
 
   request(url, function(err, resp, body){
+    if (err) return;
+
     $ = cheerio.load(body);
     // for each of our targets (within the request body)...
     $(targets).each(function(){
-      content = $(this).text();
+      var content = $(this).text();
       bukkitData.push(content);
     });
-    if (err) {
-      return;
-    }
     callback(bukkitData);
   }); // end request
 
 };
 
+exports.format = function format(array) {
+
+};
+
+/**
+ * takes an
+ * @param  {[type]} bukkits [description]
+ * @return {[type]}         [description]
+ */
+exports.store = function store (array){
+  console.log('yeah'); // pass
+  // bukkitsJson = JSON.stringify(bukkits)
+};
